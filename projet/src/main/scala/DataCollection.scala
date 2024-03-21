398e8ea80e79e33a7abd089c9dd1c4fda12be390
J-IFT <juliette.infanti@gmail.com>
2024-03-21 09:45:19 +0100

    projet fonctionnel

--- projet/src/main/scala/DataCollection.scala
+++ projet/src/main/scala/DataCollection.scala
@@ -1,18 +1,30 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkConf
 
 object DataCollection {
   def main(args: Array[String]): Unit = {
-    // Créer une session Spark
+    // Configuration Spark
+    val conf: SparkConf = new SparkConf()
+      .setMaster("local[4]")  // Utilisez 4 threads locaux
+      .setAppName("DataCollection")
+      .set("spark.driver.host", "localhost")  // Définir l'hôte du pilote
+      .set("spark.testing.memory", "2147480000")  // Définir la mémoire de test (optionnel)
+
+    // Créer une session Spark avec la configuration définie
     val spark = SparkSession.builder()
-      .appName("DataCollection")
-      .master("local")
+      .config(conf)
       .getOrCreate()
 
-    // Charger les données à partir d'un fichier texte
-    val data = spark.read.text("../data/*.txt")
+    // Charger les données à partir de chaque fichier texte
+    val data1 = spark.read.text("../data/books_large_p1.txt")
+    val data2 = spark.read.text("../data/books_large_p2.txt")
 
     // Afficher les premières lignes des données
-    data.show()
+    println("Data from file1:")
+    data1.show()
+
+    println("Data from file2:")
+    data2.show()
 
     // Autres opérations de collecte et de structuration des données
 
