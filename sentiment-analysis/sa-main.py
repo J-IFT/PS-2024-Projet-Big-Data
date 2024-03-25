d337326521a5faf3ef40b03b195d5da51357cda5
LorisPercin <74607168+LorisPercin@users.noreply.github.com>
2024-03-25 14:17:11 +0100

    🎨 improve structure of code

--- sentiment-analysis/sa-main.py
+++ sentiment-analysis/sa-main.py
@@ -2,26 +2,26 @@ import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 from nltk.tokenize import sent_tokenize
 
+
+def get_avg_compound_for_sentences(sentences):
+    compound = 0
+    for sentence in sentences:
+        compound += vader.polarity_scores(sentence)['compound']
+
+    # Arrondit à 4 chiffres pour correspondre au compound de vader
+    return round(compound / len(sentences), 4)
+
+
 # Décommenter la ligne ci dessous pour télécharger les données pour le modèle
 # nltk.download(["stopwords", "names", "state_union", "punkt"])
 
-# Récupère le fichier en texte brut
-# TODO: Remplacer avec les fichiers livres
-stateUnionRaw = nltk.corpus.state_union.raw('1945-Truman.txt')
+if __name__ == "__main__":
+    # Récupère le fichier en texte brut
+    # TODO: Remplacer avec les fichiers livres
+    stateUnionRaw = nltk.corpus.state_union.raw('1945-Truman.txt')
 
-stateUnionSentences = sent_tokenize(stateUnionRaw)
+    # Modèle prè-entrainé de NLTK
+    vader = SentimentIntensityAnalyzer()
 
-# Modèle prè-entrainé de NLTK
-vader = SentimentIntensityAnalyzer()
-
-# Moyenne des compound des phrases
-compound = 0
-for sentence in stateUnionSentences:
-    compound += vader.polarity_scores(sentence)['compound']
-compound /= len(stateUnionSentences)
-
-print("Total")
-print(vader.polarity_scores(stateUnionRaw))
-
-print("Moyenne")
-print(compound)
+    print("Total: ", vader.polarity_scores(stateUnionRaw)["compound"])
+    print("Moyenne: ", get_avg_compound_for_sentences(sent_tokenize(stateUnionRaw)))
