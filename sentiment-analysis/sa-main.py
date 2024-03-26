2805af6d020678d282da225f1c62ffb57a225e2e
LorisPercin <74607168+LorisPercin@users.noreply.github.com>
2024-03-26 14:03:30 +0100

    🏗️ combine sa files

--- sentiment-analysis/sa-main.py
+++ sentiment-analysis/sa-main.py
@@ -12,8 +12,20 @@ def get_sentences_avg_compound(sentences):
     return round(compound / len(sentences), 4)
 
 
-# Décommenter la ligne ci dessous pour télécharger les données pour le modèle
-# nltk.download(["stopwords", "names", "state_union", "punkt"])
+def skip_unwanted(pos_tuple):
+    word, tag = pos_tuple
+
+    if not word.isalpha() or word in unwanted_words:
+        return False
+
+    # NN: Noun, singular or mass
+    if tag.startswith("NN"):
+        return False
+
+    return True
+
+
+nltk.download(["stopwords", "names", "state_union", "punkt", "movie_reviews", "averaged_perceptron_tagger"])
 
 if __name__ == "__main__":
     # Récupère le fichier en texte brut
@@ -23,5 +35,34 @@ if __name__ == "__main__":
     # Modèle prè-entrainé de NLTK
     vader = SentimentIntensityAnalyzer()
 
-    print("Total: ", vader.polarity_scores(stateUnionRaw)["compound"])
-    print("Moyenne: ", get_sentences_avg_compound(sent_tokenize(stateUnionRaw)))
+    unwanted_words = nltk.corpus.stopwords.words("english")
+    unwanted_words.extend([w.lower() for w in nltk.corpus.names.words()])
+
+    # Récupère les mots à connotation neg/pos en enlevant les mots inutiles grâce aux tags "Part Of Speech"
+    positive_words = [word for word, tag in filter(
+        skip_unwanted,
+        nltk.pos_tag(nltk.corpus.movie_reviews.words(categories=["pos"]))
+    )]
+    negative_words = [word for word, tag in filter(
+        skip_unwanted,
+        nltk.pos_tag(nltk.corpus.movie_reviews.words(categories=["neg"]))
+    )]
+
+    # Frequency distribution
+    # Récupères les mots à connotation les plus présents
+    positive_fd = nltk.FreqDist(positive_words)
+    negative_fd = nltk.FreqDist(negative_words)
+
+    common_set = set(positive_fd).intersection(negative_fd)
+
+    for word in common_set:
+        del positive_fd[word]
+        del negative_fd[word]
+
+    # Les 100 mots apparaissant les plus souvent, uniquement dans des reviews positives ou négatives
+    # On est donc censé obtenir des mots à forte valeur positive/negative
+    top_100_positive = {word for word, count in positive_fd.most_common(100)}
+    top_100_negative = {word for word, count in negative_fd.most_common(100)}
+
+    print("top_100_positive: ", top_100_positive)
+    print("top_100_negative: ", top_100_negative)
