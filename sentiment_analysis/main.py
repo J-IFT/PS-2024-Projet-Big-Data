8fdf021df5c7a96cc40b55dadc1724026d975737
LorisPercin <loris.percin@outlook.fr>
2024-05-14 16:40:33 +0200

    📝 Add comments

--- sentiment_analysis/main.py
+++ sentiment_analysis/main.py
@@ -3,43 +3,32 @@ from nltk.sentiment import SentimentIntensityAnalyzer
 from random import shuffle
 from statistics import mean
 
+# Téléchargements des corpus pour entrainer notre modèle
+nltk.download([
+    "names",
+    "stopwords",
+    "movie_reviews",
+    "averaged_perceptron_tagger",
+    "vader_lexicon",
+    "punkt",
+])
 
-# nltk.download([
-#     "names",
-#     "stopwords",
-#     "state_union",
-#     "twitter_samples",
-#     "movie_reviews",
-#     "averaged_perceptron_tagger",
-#     "vader_lexicon",
-#     "punkt",
-# ])
-
-
-sia = SentimentIntensityAnalyzer()
-
+# ==== ENTRAINEMENT DU MODELE ====
+# Le but est de récolter les mots présents dans les reviews de films déjà catégoriés.
+# En comparant les deux listes de mots on peux supprimer les mots en communs pour
+# obtenir une liste de mots à forte connotation positive ou négative.
 positive_review_ids = nltk.corpus.movie_reviews.fileids(categories=["pos"])
 negative_review_ids = nltk.corpus.movie_reviews.fileids(categories=["neg"])
 all_review_ids = positive_review_ids + negative_review_ids
 
-
-def is_positive(review_id: str) -> bool:
-    """True if the average of all sentence compound scores is positive."""
-    text = nltk.corpus.movie_reviews.raw(review_id)
-    scores = [
-        sia.polarity_scores(sentence)["compound"]
-        for sentence in nltk.sent_tokenize(text)
-    ]
-    return mean(scores) > 0
-
-
-unwanted = nltk.corpus.stopwords.words("english")
-unwanted.extend([w.lower() for w in nltk.corpus.names.words()])
+# Liste de mots inutiles, qui ne serviront pas à déterminer le sentiment
+unwanted_words = nltk.corpus.stopwords.words("english")
+unwanted_words.extend([w.lower() for w in nltk.corpus.names.words()])
 
 
 def skip_unwanted(pos_tuple):
     word, tag = pos_tuple
-    if not word.isalpha() or word in unwanted:
+    if not word.isalpha() or word in unwanted_words:
         return False
     if tag.startswith("NN"):
         return False
@@ -55,11 +44,14 @@ negative_words = [word for word, tag in filter(
     nltk.pos_tag(nltk.corpus.movie_reviews.words(categories=["neg"]))
 )]
 
+# Récupères tout les mots des reviews
 positive_fd = nltk.FreqDist(positive_words)
 negative_fd = nltk.FreqDist(negative_words)
 
+# Crée une liste des mots en communs entre les reviews positives et négatives.
 common_set = set(positive_fd).intersection(negative_fd)
 
+# Supprime les mots en commun pour ne que ceux ayant une forte connotation positive ou négative.
 for word in common_set:
     del positive_fd[word]
     del negative_fd[word]
@@ -67,22 +59,11 @@ for word in common_set:
 top_100_positive = {word for word, count in positive_fd.most_common(100)}
 top_100_negative = {word for word, count in negative_fd.most_common(100)}
 
+# Modèle préentrainé de NLTK
+vader = SentimentIntensityAnalyzer()
 
 
-
-shuffle(all_review_ids)
-correct = 0
-for review_id in all_review_ids:
-    if is_positive(review_id):
-        if review_id in positive_review_ids:
-            correct += 1
-    else:
-        if review_id in negative_review_ids:
-            correct += 1
-
-print(F"{correct / len(all_review_ids):.2%} correct")
-
-
+# Crée des features pour représenter les reviews en se concentrant sur le positif.
 def extract_features(text):
     features = dict()
     wordcount = 0
@@ -93,14 +74,12 @@ def extract_features(text):
         for word in nltk.word_tokenize(sentence):
             if word.lower() in top_100_positive:
                 wordcount += 1
-        compound_scores.append(sia.polarity_scores(sentence)["compound"])
-        positive_scores.append(sia.polarity_scores(sentence)["pos"])
+        compound_scores.append(vader.polarity_scores(sentence)["compound"])
+        positive_scores.append(vader.polarity_scores(sentence)["pos"])
 
-    # Adding 1 to the final compound score to always have positive numbers
-    # since some classifiers you'll use later don't work with negative numbers.
-    features["mean_compound"] = mean(compound_scores) + 1
-    features["mean_positive"] = mean(positive_scores)
-    features["wordcount"] = wordcount
+    features["mean_compound"] = mean(compound_scores) + 1   # moyenne du sentiment général des phrases de la review
+    features["mean_positive"] = mean(positive_scores)   # moyenne des scores de positivités des phrases
+    features["wordcount"] = wordcount   # Nombres de mots du top 100 positif apparaissant dans la review
 
     return features
 
@@ -114,37 +93,10 @@ features.extend([
     for review in nltk.corpus.movie_reviews.fileids(categories=["neg"])
 ])
 
-
+# Entrainement du modèle Vader à partir de nos nouvelles données
 train_count = len(features) // 4
 shuffle(features)
 classifier = nltk.NaiveBayesClassifier.train(features[:train_count])
-print(classifier.show_most_informative_features(10))
-print(nltk.classify.accuracy(classifier, features[train_count:]))
 
-# data = [
-#     ("I love this book", "positive"),
-#     ("This book is terrible", "negative")
-# ]
-#
-#
-# def preprocess(text):
-#     tokens = word_tokenize(text)
-#     tokens = [token.lower() for token in tokens if token.isalpha()]
-#     tokens = [token for token in tokens if token not in stopwords.words('english')]
-#     return tokens
-#
-#
-# # Convert data to features
-# featuresets = [(preprocess(text), sentiment) for (text, sentiment) in data]
-#
-# # Split the data into training and testing sets
-# train_set, test_set = featuresets[50:], featuresets[:50]
-#
-# # Train a Naive Bayes classifier
-# classifier = NaiveBayesClassifier.train(train_set)
-#
-# # Test the classifier
-# print("Accuracy:", nltk_accuracy(classifier, test_set))
-#
-# # Example usage
-# print(classifier.classify(preprocess("I really enjoyed this book")))
+# Précision du nouveau modèle
+print(nltk.classify.accuracy(classifier, features[train_count:]))
